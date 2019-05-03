4ef704e5359a1805bff1b4a8f4d24f125f17d375
asuluntas <aysunsulun@gmail.com>
2019-05-03 12:06:41 -0700

    Changed port

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const db = require('./db/index');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(bodyParser.urlencoded( { extended: true } ))
 app.use(bodyParser.json())
