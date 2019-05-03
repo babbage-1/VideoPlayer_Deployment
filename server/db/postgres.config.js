b903589ba71c1fda9b5bb08e42bcdebe34029c60
asuluntas <aysunsulun@gmail.com>
2019-05-03 16:46:36 -0700

    Changed config

--- server/db/postgres.config.js
+++ server/db/postgres.config.js
@@ -3,8 +3,7 @@ const config = {
   host: 'ec2-54-67-84-44.us-west-1.compute.amazonaws.com',
   database: 'postgres',
   password: 'aysun4793',
-  port: 5432,
-  max: 15
+  port: 5432
 };
 
 module.exports = {
